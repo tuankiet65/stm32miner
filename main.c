93124336ed26f4b1dc1604d5850a56c3da449d94
Ho Tuan Kiet <tuankiet65@gmail.com>
2018-06-15 06:25:04 +0700

    main: replace 'finished' with 'state'
    state variable will now provide information on the current state of the worker
    (ready/working/found nonce/nonce not found/error (reserved))
    
    Also adopt the new i2c helper function.

--- main.c
+++ main.c
@@ -31,35 +31,38 @@
 uint32_t header[40];
 
 enum i2c_variable_ids {
+    version = 0,
+    state,
+    hashrate,
+    current_job_id,
+    winning_nonce,
+
     new_job_id,
     new_header,
     execute_job,
-    force_calibrate,
-
-    version,
-    hashrate,
-    current_job_id,
-    finished,
-    winning_nonce
+    force_calibrate
 };
 
-struct i2c_variable i2c_variables[] = {
-    { .id = new_job_id     , .size = sizeof(uint8_t), .rw = I2C_RW },
-    { .id = new_header     , .size = sizeof(uint32_t[20]) , .rw = I2C_RW },
-    { .id = execute_job    , .size = sizeof(uint8_t), .rw = I2C_RW },
-    { .id = force_calibrate, .size = sizeof(uint8_t), .rw = I2C_RW },
+#define STATE_READY 0x01
+#define STATE_WORKING 0x02
+#define STATE_FOUND 0x03
+#define STATE_NOT_FOUND 0x04
+#define STATE_ERROR 0xff
 
-    { .id = version        , .size = sizeof(char[8])      , .rw = I2C_RO },
-    { .id = hashrate       , .size = sizeof(uint32_t)     , .rw = I2C_RO },
-    { .id = current_job_id , .size = sizeof(uint8_t), .rw = I2C_RO },
-    { .id = finished       , .size = sizeof(uint8_t), .rw = I2C_RO },
-    { .id = winning_nonce  , .size = sizeof(uint32_t)     , .rw = I2C_RO },
+struct i2c_variable i2c_variables[] = {
+    { .id = version        , .size = sizeof(char[8])     , .rw = I2C_RO },
+    { .id = state          , .size = sizeof(uint8_t)     , .rw = I2C_RO },
+    { .id = hashrate       , .size = sizeof(uint32_t)    , .rw = I2C_RO },
+    { .id = current_job_id , .size = sizeof(uint8_t)     , .rw = I2C_RO },
+    { .id = winning_nonce  , .size = sizeof(uint32_t)    , .rw = I2C_RO },
+
+    { .id = new_job_id     , .size = sizeof(uint8_t)     , .rw = I2C_RW },
+    { .id = new_header     , .size = sizeof(uint32_t[20]), .rw = I2C_RW },
+    { .id = execute_job    , .size = sizeof(uint8_t)     , .rw = I2C_RW },
+    { .id = force_calibrate, .size = sizeof(uint8_t)     , .rw = I2C_RW },
 };
 
 volatile uint8_t new_data = 0;
-
-uint8_t ZERO = 0;
-uint8_t ONE = 1;
 
 void write_callback() {
     i2c_read(execute_job, &new_data);
@@ -68,10 +71,11 @@ void write_callback() {
 int main() {
     rcc_clock_setup_in_hsi_out_64mhz();
     log_init();
-
+    
     i2c_init(get_address(), 64, i2c_variables, sizeof(i2c_variables) / sizeof(struct i2c_variable));
     i2c_register_write_callback(write_callback);
     i2c_write(version, GIT_VERSION);
+    i2c_write_uint8(state, STATE_READY);
 
     LOG(INFO, "stm32miner, commit "GIT_VERSION);
     LOG(INFO, "Ready, waiting for job");
@@ -82,9 +86,13 @@ int main() {
         // interrupt, or race condition will happen
         if (new_data) {
             CM_ATOMIC_BLOCK() {
+                i2c_write_uint8(execute_job, 0);
+                i2c_write_uint8(current_job_id, 0);
+                i2c_write_uint32(hashrate, 0);
+                i2c_write_uint32(winning_nonce, 0);
+
                 LOG(INFO, "New job");
                 new_data = 0;
-                i2c_write(execute_job, &ZERO);
 
                 i2c_read(new_header, header);
                 // SHA-256 padding
@@ -100,15 +108,19 @@ int main() {
                 i2c_read(new_job_id, &new_job_id);
                 i2c_write(current_job_id, &new_job_id);
                 
-                i2c_write(finished, &ZERO);
+                i2c_write_uint8(state, STATE_WORKING);
                 i2c_dump();
             }
 
             uint32_t result;
             if (scanhash_sha256d(header, &result, &new_data)) {
-                i2c_write(finished, &ONE);
+                i2c_write_uint8(state, STATE_FOUND);
                 i2c_write(winning_nonce, &result);
+            } else {
+                i2c_write_uint8(state, STATE_NOT_FOUND);
             }
         }
     }
+
+    return 0;
 }
